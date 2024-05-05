b04dd4001b221ca34e5f6792ba7707b533c7eda9
Vyacheslav Nikolaev <slavkapiyavka@yandex.ru>
2024-05-05 20:01:55 +0200

    feat; display title (page or 'шпора') in header

--- src/components/header/HeaderComponent.tsx
+++ src/components/header/HeaderComponent.tsx
@@ -1,14 +1,19 @@
+'use client'
+
 import Link from "next/link";
 import "./style.css";
+import {topics} from "@/shared/topics";
+import {usePathname} from "next/navigation";
 
 export default function HeaderComponent() {
-    const pageTitle = 'pageTitle'
+    const p = usePathname()
+    const pageTitle: any = topics.find((topic) => topic.link === p.replace('/', ''))
 
     return (
         <header className="main-header">
             <Link href={'/'}>на главную</Link>
 
-            <h1>{ pageTitle }</h1>
+            <h1 className="main-header__title overflow-ellipsis">{ pageTitle ? pageTitle.label : 'Шпора' }</h1>
         </header>
     )
 }
