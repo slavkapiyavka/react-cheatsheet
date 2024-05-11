d1aed555c6859b44f750eed554935db6fe02cd0e
Vyacheslav Nikolaev <slavkapiyavka@yandex.ru>
2024-05-11 21:24:36 +0200

    fix: HOC typo

--- src/app/hoc/page.tsx
+++ src/app/hoc/page.tsx
@@ -1,7 +1,7 @@
 export default function HOC() {
     return (
         <div className="content">
-            <h1>hoc - high order component, функция высшего порядка</h1>
+            <h1>hoc - higher order component, функция высшего порядка</h1>
             <div>
                 <p>HOC - так же, как и функция в обычном JS, это компонент,который:</p>
                 <ul>
