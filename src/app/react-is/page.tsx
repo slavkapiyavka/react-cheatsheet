0b39661b27e069742236c21fb65d18ed1905e679
Vyacheslav Nikolaev <slavkapiyavka@yandex.ru>
2024-05-12 03:24:29 +0200

    feat: add short React description

--- src/app/react-is/page.tsx
+++ src/app/react-is/page.tsx
@@ -1,4 +1,5 @@
 import {Metadata} from "next";
+import {ReactElement} from "react";
 
 export function generateMetadata(): Metadata {
     return {
@@ -6,11 +7,31 @@ export function generateMetadata(): Metadata {
     }
 }
 
-export default function ReactIs() {
+export default function ReactIs(): ReactElement | null {
     return (
         <>
             <h1>react - это...</h1>
-            <p>библиотека JavaScript для создания UI (user interface), разработанная командой Facebook, и она уже давно завоевала популярность среди разработчиков по всему миру.</p>
+            <p>библиотека JavaScript для создания UI, пользовательских интерфейсов, разработанная командой Facebook, и она уже давно завоевала популярность среди разработчиков по всему миру.</p>
+
+            <p>преимущества</p>
+            <ul>
+                <li>
+                    <p>Очень просто сделать небольшое приложение. Если ты знаешь JS - тебе не составит труда писать на React.</p>
+                    <p></p>
+                </li>
+                <li>
+                    <p>Декларативность</p>
+                </li>
+                <li>
+                    <p>Компонентный подход</p>
+                </li>
+                <li>
+                    <p>Популярность и стабильность</p>
+                </li>
+            </ul>
+
+            <h2>Next.js</h2>
+            <p>это уже фреймворк, построенный на базе react. Внутри за UI по прежнему отвечает React, помимо него ещё добавлен роутинг, SSR, SSG и прочее</p>
         </>
     )
 }
