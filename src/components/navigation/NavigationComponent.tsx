d08338e7625c2944718c4539bfe44fa090ae7707
slavkapiyavka <slavkapiyavka@yandex.ru>
2025-12-04 02:01:57 +0300

    chore: improve links styles, disable empty pages links, fix types, run lint

--- src/components/navigation/NavigationComponent.tsx
+++ src/components/navigation/NavigationComponent.tsx
@@ -1,26 +1,31 @@
-'use client'
+"use client";
 
-import Link from "next/link"
-import { usePathname } from "next/navigation"
-import { topics } from "@/shared/topics";
-import "./style.css"
-
-type TopicElement = {label: string, link: string}
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { topics, Topic } from "@/shared/topics";
+import "./style.css";
 
 export default function NavigationComponent() {
-    const pathName = usePathname()
+  const pathName = usePathname();
 
-    return (
-        <nav className="main-nav">
-            <ul className="nav-list">
-                {topics.map((topic: TopicElement) => (
-                    <li key={topic.link} className="nav-list-element">
-                        <Link href={topic.link} className={`link${pathName === `/${topic.link}` ? ' link_active' : ''}`}>
-                            {topic.label}
-                        </Link>
-                    </li>
-                ))}
-            </ul>
-        </nav>
-    )
+  return (
+    <nav className="main-nav">
+      <ul className="nav-list">
+        {topics.map((topic: Topic) => (
+          <li
+            key={topic.link}
+            className="nav-list-element"
+            inert={!topic.isAvailable}
+          >
+            <Link
+              href={topic.link}
+              className={`link${pathName === `/${topic.link}` ? " link_active" : ""}`}
+            >
+              {topic.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 }
